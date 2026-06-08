4e173cbcb0c481f42968a328e25296de706a1ea9
kgb <kissgaborbalazs@gmail.com>
2026-06-08 23:47:48 +0200

    Fix relative paths for GitHub Pages subdirectory

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,12 @@
 const CACHE = 'hazipenz-v1';
-const ASSETS = ['/', '/index.html', '/css/style.css', '/js/app.js', '/js/db.js', '/manifest.json'];
+const ASSETS = [
+    './',
+    './index.html',
+    './css/style.css',
+    './js/app.js',
+    './js/db.js',
+    './manifest.json'
+];
 
 self.addEventListener('install', e =>
     e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)))
